d49728469a09c647bde1d03710632d79a8c22d5d
Evan Stanton <evan.h.stanton@gmail.com>
2024-03-25 10:13:46 +0100

    Add markdown preview plugin.
    Signed-off-by: Evan Stanton <evan.h.stanton@gmail.com>

--- init.lua
+++ init.lua
@@ -777,6 +777,16 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
+
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
